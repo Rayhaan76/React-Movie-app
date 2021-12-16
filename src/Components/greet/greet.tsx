7cf592e42fbd80bea8462ae5b320a2fd852f7e86
rayhaan76 <rayhaan_al@hotmail.co.uk>
2021-12-16 20:16:20 +0000

    maybe

--- src/Components/greet/greet.tsx
+++ src/Components/greet/greet.tsx
@@ -1,11 +1,15 @@
 type FullListProps = {
-    fullList: Object
+    fullList: any
 }
 
+
+
 export const Greet = (props: FullListProps) => {
+    const name = props.fullList.id;
+
     return (
         <div>
-            <h2>hello</h2>
+            <h2>hello {props.fullList[1].title}</h2>
             {console.log(props.fullList)}
         </div>
     )
