93762d84655cd475581d29aced26c0f0ef972892
rayhaan76 <rayhaan_al@hotmail.co.uk>
2021-12-17 18:29:43 +0000

    rating toggle

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,24 +1,27 @@
-import React, { FC, useState } from 'react';
+import React, { useState } from 'react';
 
-import { FullList } from '../SearchResults/SearchResults';
+
+import { SearchResults } from '../SearchResults/SearchResults';
+
 import './App.css';
+
 
 
 const App = () => {
 
   const [fullList, setFullList] = useState([
-    {id: 1, title: 'Movie 1', rating: 60},
-    {id: 2, title: 'Movie 2', rating: 70},
-    {id: 3, title: 'Movie 3', rating: 50},
-    {id: 4, title: 'Movie 4', rating: 100},
+    {id: 1, title: 'A Movie 1', rating: 60},
+    {id: 2, title: 'B Movie 2', rating: 70},
+    {id: 3, title: 'C Movie 3', rating: 50},
+    {id: 4, title: 'D Movie 4', rating: 100},
   ]);
-
+  
   return (
     <div className="App">
       <header className="App-header">
         Comedy Movies
       </header>
-        <FullList fullList={fullList}/>
+        <SearchResults fullList={fullList} setFullList={setFullList}/>
       <div>
 
       </div>
--- src/Components/SearchResults/SearchResults.tsx
+++ src/Components/SearchResults/SearchResults.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 
 type FullListProps = {
     fullList: {
@@ -5,18 +6,64 @@ type FullListProps = {
         title: string
         rating: number
     }[]
+    setFullList: React.Dispatch<React.SetStateAction<{
+        id: number;
+        title: string;
+        rating: number;
+    }[]>>
 }
 
-export const FullList = (props: FullListProps) => {
+export const SearchResults = (props: FullListProps) => {
     
+    const [searchTerm, setSearchTerm] = useState('');
+    const [toggle, setToggle] = useState<boolean>(false)
+
+
+    const searchMovies = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value);
+        setSearchTerm(event.target.value);
+        
+    }
+
+    const sortRating = () => {
+        if(toggle === false) {
+            const sorted = [...props.fullList].sort((a, b) => b.rating - a.rating);
+            props.setFullList(sorted);
+            setToggle(true);
+            console.log(toggle);
+        } else if (toggle === true) {
+            const sorted = [...props.fullList].sort((a, b) => a.rating - b.rating);
+            props.setFullList(sorted);
+            setToggle(false);
+            console.log(toggle);
+        } 
+    }
 
     return (
         <div>
-            {props.fullList.map(movie => {
-                return(
-                    <p>id: {movie.id} Title: {movie.title} Rating: {movie.rating}</p>
-                )
-            })}
+
+            <input 
+                type='text'
+                placeholder="Search"
+                onChange={searchMovies}
+            />
+
+            <button onClick={sortRating}> Sort by rating</button>
+
+            <div>
+                {props.fullList.filter((movieList) => {
+                    if (searchTerm === '') {
+                        return movieList;
+                    } else if (movieList.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        return movieList
+                    } 
+                    return console.log('error')
+                }).map((val, key) => {
+                    return(
+                        <p>ID: {val.id} TITLE: {val.title} RATING: {val.rating}</p>
+                    )
+                })}
+            </div>
         </div>
     )
 }
