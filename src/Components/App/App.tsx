d42f6e3b9718507d5969105ce83105bd054b8681
rayhaan76 <rayhaan_al@hotmail.co.uk>
2021-12-19 15:49:07 +0000

    All working

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,64 +1,63 @@
 import React, { useEffect, useState } from 'react';
 
-import { getComedyMovies2 } from '../Moviedb/Moviedb';
 import { SearchResults } from '../SearchResults/SearchResults';
 
 import './App.css';
 
 const App = () => {
   
-  //const [allComedyMovies2, setAllComedyMovies2] = useState<any>([]);
-
-  // const loadData = async () => {
-  //   const testing = await getComedyMovies();
-  //   console.log('testing');
-  //   setAllComedyMovies(testing)
-  //   console.log(testing)
-  // }
-
-  // loadData();
-
-  // useEffect(() => {
-  //   loadData();
-  // }, [])
+  //NOTE: Sometimes the sorting can cause a movie to get stuck at the top of the list. 
+  //I do not know what causes it, however it only seems to happen rarely. 
 
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
+    let array: any = [];
+
     fetch('https://api.themoviedb.org/3/discover/movie?api_key=30681930d9f8a4d49ec75249be9647a1&with_genres=35&page=1')
       .then((res) => res.json())
-      .then((data) => setMovies(data.results));
+      .then((data) => {
+        array.push(...data.results);
+      });
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=30681930d9f8a4d49ec75249be9647a1&with_genres=35&page=2')
+      .then((res) => res.json())
+      .then((data) => {
+        array.push(...data.results);
+      });
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=30681930d9f8a4d49ec75249be9647a1&with_genres=35&page=3')
+      .then((res) => res.json())
+      .then((data) => {
+        array.push(...data.results);
+      });
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=30681930d9f8a4d49ec75249be9647a1&with_genres=35&page=4')
+      .then((res) => res.json())
+      .then((data) => {
+        array.push(...data.results);
+      });
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=30681930d9f8a4d49ec75249be9647a1&with_genres=35&page=5')
+      .then((res) => res.json())
+      .then((data) => {
+        array.push(...data.results);
+        setMovies(array);
+      });
   }, [])
-
-  // useEffect(() => {
-  //   let data = [];
-  //   data = getComedyMovies2();
-  //   console.log(data);
-  //   setMovies(data);
-  //   console.log(data);
-  // }, [])
 
   if (movies === undefined) {
     return <div>Loading</div>
   }
 
   return (
-    <div>
+    <div className='App'>
+
+      <header className='App-header'>
+        Popular Comedy Movies
+      </header>
+
       <SearchResults movies={movies} setMovies={setMovies}/>
+      
     </div>
   )
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       Comedy Movies
-  //     </header>
-  //       <SearchResults />
-  //     <div>
-
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default App;
--- src/Components/SearchResults/SearchResults.tsx
+++ src/Components/SearchResults/SearchResults.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode, useState, useEffect } from "react"
+import React, { useState } from "react"
 
-import {getComedyMovies} from '../Moviedb/Moviedb'
+import './SearchResults.css'
 
 type MovieProps = {
     movies: any
@@ -9,21 +9,8 @@ type MovieProps = {
 
 export const SearchResults = (props: MovieProps) => {
     
-    const [allComedyMovies, setAllComedyMovies] = useState<any>([]);
-
-    const loadData = async () => {
-      const testing = await getComedyMovies();
-      console.log(testing);
-      setAllComedyMovies(testing);
-      console.log(testing);
-    }
-    
-    useEffect(() => {
-        loadData();
-    }, [])
-
     const [searchTerm, setSearchTerm] = useState('');
-    const [toggle, setToggle] = useState<boolean>(false);
+    const [toggle, setToggle] = useState<boolean | undefined>(undefined);
 
     const searchMovies = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(event.target.value);
@@ -31,57 +18,68 @@ export const SearchResults = (props: MovieProps) => {
     }
 
     const sortRating = () => {
-        if(toggle === false) {
+        if(toggle === false || toggle === undefined) {
             const sorted = [...props.movies].sort((a, b) => b.vote_average - a.vote_average);
             props.setMovies(sorted);
             setToggle(true);
-            console.log(toggle);
         } else if (toggle === true) {
             const sorted = [...props.movies].sort((a, b) => a.vote_average - b.vote_average);
             props.setMovies(sorted);
             setToggle(false);
-            console.log(toggle);
         } 
     }
+    //console.log(toggle);
+    //console.log(props.movies);
 
-    console.log(props.movies);
+    let button;
 
-    // if (allComedyMovies.length === 0) {
-    //     return (
-    //         <div>
-    //             loading
-    //         </div>
-    //     )
-    // }
+    if(toggle === undefined) {
+        button = <span>Sort by Rating</span>
+    } else if (toggle === true) {
+        button = <span>Sort by Rating: Asc</span>
+    } else if (toggle === false) {
+        button = <span>Sort by Rating: Desc</span>
+    }
 
     return (
         <div>
 
             <input 
                 type='text'
-                placeholder="Search"
+                placeholder="Search..."
                 onChange={searchMovies}
+                className="Search-input"
             />
 
-            <button onClick={sortRating}> Sort by rating</button>
-
-            <div>
-                {/* {props.movies[1].title} */}
-            </div>
+            <button className="Sort-button" onClick={sortRating}>{button}</button>
             
-            <div>
-                {props.movies.filter((movieList: { title: string; }) => {
-                    if (searchTerm === '') {
-                        return movieList;
-                    } else if (movieList.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        return movieList
-                    } 
-                    return console.log('no filter');
-                }).map((val: {title: string; vote_average: number}, key: number) => {
-                    return(
-                        <p>TITLE: {val.title} RATING: {val.vote_average}</p>
-                    )
-                })}
+            <div className="List-container">
+                <table className="Movie-table">
+                    <tbody>
+                    <tr>
+                        
+                        <th className="Title-header">Title</th>
+                        <th className="Rating-header" onClick={sortRating}>Rating</th>
+                    </tr>
+
+                    {props.movies.filter((movieList: { title: string; }) => {
+                        if (searchTerm === '') {
+                            return movieList;
+                        } else if (movieList.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return movieList
+                        } 
+                        return console.log('no filter');
+                    }).map((val: {id: number; title: string; vote_average: number}, key: number) => {
+                        return(
+                            <tr key={val.id}>
+                                
+                                <td className="Title-data" key={val.title}>{val.title}</td>
+                                <td className="Rating-data" key={val.vote_average}>{val.vote_average}</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>        
+                </table>
             </div>
         </div>
     )
